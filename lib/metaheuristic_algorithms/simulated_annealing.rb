719a4ec3b8ad93220bb72b88964253ec51c4b3f8
Tadatoshi Takahashi <tadatoshi@gmail.com>
2015-10-24 15:19:05 -0400

    SimulatedAnnealing: Converted BigDecimal to Float.

--- lib/metaheuristic_algorithms/simulated_annealing.rb
+++ lib/metaheuristic_algorithms/simulated_annealing.rb
@@ -1,4 +1,4 @@
-require 'bigdecimal/math'
+# require 'bigdecimal/math'
 # In order to support JRuby, decided not to use distribution gem:
 # require 'distribution'
 
@@ -19,21 +19,32 @@ module MetaheuristicAlgorithms
                              end        
     end
 
-    def search(temperature: BigDecimal('100.0'), minimal_temperature: BigDecimal('1'), maximum_number_of_rejections: 2500, 
+    # def search(temperature: BigDecimal('100.0'), minimal_temperature: BigDecimal('1'), maximum_number_of_rejections: 2500, 
+    #            maximum_number_of_runs: 500, maximum_number_of_acceptances: 15, 
+    #            bolzmann_constant: BigDecimal('1'), cooling_factor: BigDecimal('0.95'), energy_norm: BigDecimal('10'), 
+    #            standard_diviation_for_estimation: BigDecimal('6'), ratio_of_energy_delta_over_evaluation_delta: BigDecimal('10'))
+    def search(temperature: 100.0, minimal_temperature: 1, maximum_number_of_rejections: 2500, 
                maximum_number_of_runs: 500, maximum_number_of_acceptances: 15, 
-               bolzmann_constant: BigDecimal('1'), cooling_factor: BigDecimal('0.95'), energy_norm: BigDecimal('10'), 
-               standard_diviation_for_estimation: BigDecimal('6'), ratio_of_energy_delta_over_evaluation_delta: BigDecimal('10'))
+               bolzmann_constant: 1, cooling_factor: 0.95, energy_norm: 10, 
+               standard_diviation_for_estimation: 6, ratio_of_energy_delta_over_evaluation_delta: 10)
 
-      temperature = BigDecimal(temperature.to_s) unless temperature.kind_of?(BigDecimal)
-      minimal_temperature = BigDecimal(minimal_temperature.to_s) unless minimal_temperature.kind_of?(BigDecimal)
+      # temperature = BigDecimal(temperature.to_s) unless temperature.kind_of?(BigDecimal)
+      # minimal_temperature = BigDecimal(minimal_temperature.to_s) unless minimal_temperature.kind_of?(BigDecimal)
+      temperature = temperature.to_f unless temperature.kind_of?(Float)
+      minimal_temperature = minimal_temperature.to_f unless minimal_temperature.kind_of?(Float)      
       maximum_number_of_rejections = maximum_number_of_rejections.to_i unless maximum_number_of_rejections.kind_of?(Integer)
       maximum_number_of_runs = maximum_number_of_runs.to_i unless maximum_number_of_runs.kind_of?(Integer)
       maximum_number_of_acceptances = maximum_number_of_acceptances.to_i unless maximum_number_of_acceptances.kind_of?(Integer)
-      bolzmann_constant = BigDecimal(bolzmann_constant.to_s) unless bolzmann_constant.kind_of?(BigDecimal)
-      cooling_factor = BigDecimal(cooling_factor.to_s) unless cooling_factor.kind_of?(BigDecimal)
-      energy_norm = BigDecimal(energy_norm.to_s) unless energy_norm.kind_of?(BigDecimal)
-      standard_diviation_for_estimation = BigDecimal(standard_diviation_for_estimation.to_s) unless standard_diviation_for_estimation.kind_of?(BigDecimal)
-      ratio_of_energy_delta_over_evaluation_delta = BigDecimal(ratio_of_energy_delta_over_evaluation_delta.to_s) unless ratio_of_energy_delta_over_evaluation_delta.kind_of?(BigDecimal)
+      # bolzmann_constant = BigDecimal(bolzmann_constant.to_s) unless bolzmann_constant.kind_of?(BigDecimal)
+      # cooling_factor = BigDecimal(cooling_factor.to_s) unless cooling_factor.kind_of?(BigDecimal)
+      # energy_norm = BigDecimal(energy_norm.to_s) unless energy_norm.kind_of?(BigDecimal)
+      # standard_diviation_for_estimation = BigDecimal(standard_diviation_for_estimation.to_s) unless standard_diviation_for_estimation.kind_of?(BigDecimal)
+      # ratio_of_energy_delta_over_evaluation_delta = BigDecimal(ratio_of_energy_delta_over_evaluation_delta.to_s) unless ratio_of_energy_delta_over_evaluation_delta.kind_of?(BigDecimal)
+      bolzmann_constant = bolzmann_constant.to_f unless bolzmann_constant.kind_of?(Float)
+      cooling_factor = cooling_factor.to_f unless cooling_factor.kind_of?(Float)
+      energy_norm = energy_norm.to_f unless energy_norm.kind_of?(Float)
+      standard_diviation_for_estimation = standard_diviation_for_estimation.to_f unless standard_diviation_for_estimation.kind_of?(Float)
+      ratio_of_energy_delta_over_evaluation_delta = ratio_of_energy_delta_over_evaluation_delta.to_f unless ratio_of_energy_delta_over_evaluation_delta.kind_of?(Float)
 
       number_of_runs = 0
       number_of_rejections = 0
@@ -68,13 +79,15 @@ module MetaheuristicAlgorithms
         # Accept if improved:
         # When objective = :maximization, "if evaluation_delta > 0 && evaluation_delta > energy_norm"
         # When objective = :minimization, "if evaluation_delta < 0 && -evaluation_delta > energy_norm"
-        if evaluation_delta.send(@objective_comparison_operator, BigDecimal('0')) && evaluation_delta.abs > energy_norm
+        # if evaluation_delta.send(@objective_comparison_operator, BigDecimal('0')) && evaluation_delta.abs > energy_norm
+        if evaluation_delta.send(@objective_comparison_operator, 0) && evaluation_delta.abs > energy_norm
           best_solution = new_estimates
           best_evaluation = new_evaluation
           number_of_acceptances = number_of_acceptances + 1
           number_of_rejections = 0
         # Accept with a small probability if not improved
-        elsif acceptance_probability(evaluation_delta, temperature, bolzmann_constant) > bigdecimal_rand
+        # elsif acceptance_probability(evaluation_delta, temperature, bolzmann_constant) > bigdecimal_rand
+      elsif acceptance_probability(evaluation_delta, temperature, bolzmann_constant) > rand
           best_solution = new_estimates
           best_evaluation = new_evaluation
           number_of_acceptances = number_of_acceptances + 1            
@@ -94,7 +107,8 @@ module MetaheuristicAlgorithms
         (0...@number_of_variables).map do |variable_index|
 
           # The value out-of-range in order to enter while loop
-          new_estimate = @function_wrapper.miminum_decision_variable_values[variable_index] - BigDecimal('1')
+          # new_estimate = @function_wrapper.miminum_decision_variable_values[variable_index] - BigDecimal('1')
+          new_estimate = @function_wrapper.miminum_decision_variable_values[variable_index] - 1
 
           while new_estimate < @function_wrapper.miminum_decision_variable_values[variable_index] || new_estimate > @function_wrapper.maximum_decision_variable_values[variable_index]
 
@@ -102,7 +116,8 @@ module MetaheuristicAlgorithms
             # But in our case, the value range is different. 
             # In order to support JRuby, decided not to use Distribution::Normal.rng:
             # new_estimate = BigDecimal(Distribution::Normal.rng(previous_estimates[variable_index], standard_diviation_for_estimation).call.to_s)
-            new_estimate = BigDecimal(gaussian(previous_estimates[variable_index], standard_diviation_for_estimation).to_s)
+            # new_estimate = BigDecimal(gaussian(previous_estimates[variable_index], standard_diviation_for_estimation).to_s)
+            new_estimate = gaussian(previous_estimates[variable_index], standard_diviation_for_estimation)
 
           end
 
--- spec/metaheuristic_algorithms/simulated_annealing_spec.rb
+++ spec/metaheuristic_algorithms/simulated_annealing_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-require 'bigdecimal'
+# require 'bigdecimal'
 
 describe MetaheuristicAlgorithms::SimulatedAnnealing do
 
@@ -11,9 +11,11 @@ describe MetaheuristicAlgorithms::SimulatedAnnealing do
 
     simulated_annealing = MetaheuristicAlgorithms::SimulatedAnnealing.new(rosenbrook_function_wrapper, number_of_variables: 2, objective: :minimization)
 
-    temperature = BigDecimal('1.0')
+    # temperature = BigDecimal('1.0')
+    temperature = 1.0
     # minimal_temperature = BigDecimal('1e-10') # Final stopping temperature
-    minimal_temperature = BigDecimal('1e-1') # Final stopping temperature
+    # minimal_temperature = BigDecimal('1e-1') # Final stopping temperature
+    minimal_temperature = 1e-1 # Final stopping temperature
     # minimal_function_value = BigDecimal('-1E+100')
     # maximum_number_of_rejections = 2500
     # maximum_number_of_rejections = 1000
@@ -24,11 +26,16 @@ describe MetaheuristicAlgorithms::SimulatedAnnealing do
     # maximum_number_of_acceptances = 5
     # maximum_number_of_acceptances = 15
     maximum_number_of_acceptances = 5
-    bolzmann_constant = BigDecimal('1')
-    cooling_factor = BigDecimal('0.95')
-    energy_norm = BigDecimal('1e-1')
-    standard_diviation_for_estimation = BigDecimal('1')  
-    ratio_of_energy_delta_over_evaluation_delta = BigDecimal('1')
+    # bolzmann_constant = BigDecimal('1')
+    bolzmann_constant = 1
+    # cooling_factor = BigDecimal('0.95')
+    cooling_factor = 0.95
+    # energy_norm = BigDecimal('1e-1')
+    energy_norm = 1e-1
+    # standard_diviation_for_estimation = BigDecimal('1')
+    standard_diviation_for_estimation = 1  
+    # ratio_of_energy_delta_over_evaluation_delta = BigDecimal('1')
+    ratio_of_energy_delta_over_evaluation_delta = 1
 
     result = simulated_annealing.search(temperature: temperature, minimal_temperature: minimal_temperature, 
                                         maximum_number_of_rejections: maximum_number_of_rejections, maximum_number_of_runs: maximum_number_of_runs, 
@@ -37,9 +44,12 @@ describe MetaheuristicAlgorithms::SimulatedAnnealing do
                                         standard_diviation_for_estimation: standard_diviation_for_estimation, ratio_of_energy_delta_over_evaluation_delta: ratio_of_energy_delta_over_evaluation_delta)
 
     # pending('Improve the code to converge toward the expected value')
-    expect(result[:best_decision_variable_values][0]).to be_within(1).of(BigDecimal('1.0112'))
-    expect(result[:best_decision_variable_values][1]).to be_within(1).of(BigDecimal('0.9988')) 
-    expect(result[:best_objective_function_value]).to be_within(1).of(BigDecimal('0.0563'))     
+    # expect(result[:best_decision_variable_values][0]).to be_within(1).of(BigDecimal('1.0112'))
+    # expect(result[:best_decision_variable_values][1]).to be_within(1).of(BigDecimal('0.9988')) 
+    # expect(result[:best_objective_function_value]).to be_within(1).of(BigDecimal('0.0563'))  
+    expect(result[:best_decision_variable_values][0]).to be_within(1).of(1.0112)
+    expect(result[:best_decision_variable_values][1]).to be_within(1).of(0.9988) 
+    expect(result[:best_objective_function_value]).to be_within(1).of(0.0563)   
 
   end
 
